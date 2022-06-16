4135bd2cb3879f2905cd06de1009f9fddede84e9
agusbayu13 <abay@students.unnes.ac.id>
2022-06-16 14:19:54 +0700

    penambahan fitur export data peserta ke excel

--- resources/views/layouts/layouts-public.blade.php
+++ resources/views/layouts/layouts-public.blade.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8">
-    <title>Minerva: Bootcamp Online dan Kelas Online Bersertifikat</title>
+    <title>Minerva: Event Online dan Kelas Online Bersertifikat</title>
     <meta content="width=device-width, initial-scale=1.0" name="viewport">
     <meta content="" name="keywords">
     <meta content="" name="description">
